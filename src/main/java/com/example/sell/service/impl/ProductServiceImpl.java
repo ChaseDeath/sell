133b962e7579e9f7ec4b4bae6c834023afc41c0c
lfx <1274740448@qq.com>
2019-03-14 23:57:27 +0800

    完成了订单的动能

--- src/main/java/com/example/sell/service/impl/ProductServiceImpl.java
+++ src/main/java/com/example/sell/service/impl/ProductServiceImpl.java
@@ -1,13 +1,17 @@
 package com.example.sell.service.impl;
 
+import com.example.sell.dto.CartDTO;
 import com.example.sell.enums.ProductStatusEnum;
 import com.example.sell.dataobject.ProductInfo;
+import com.example.sell.enums.ResultEnum;
+import com.example.sell.exception.SellException;
 import com.example.sell.repository.ProductInfoRepository;
 import com.example.sell.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -41,5 +45,30 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public ProductInfo save(ProductInfo productInfo) {
         return repository.save(productInfo);
+    }
+
+    @Override
+    public void increaseStock(List<CartDTO> cartDTOList) {
+
+    }
+
+    @Override
+    @Transactional
+    public void decreaseStock(List<CartDTO> cartDTOList) {
+
+        for(CartDTO cartDTO: cartDTOList) {
+            Optional<ProductInfo> productInfo = repository.findById(cartDTO.getProductId());
+            if(productInfo.get() == null) {
+                throw new SellException(ResultEnum.PRODUCT_NOT_EXIST);
+            }
+            Integer result = productInfo.get().getProductStock() - cartDTO.getProductQuantity();
+
+            if(result < 0) {
+                throw new SellException(ResultEnum.PRODUCT_STOCK_ERROR);
+            }
+
+            productInfo.get().setProductStock(result);
+            repository.save(productInfo.get());
+        }
     }
 }
